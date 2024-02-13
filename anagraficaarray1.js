bfa858be9bae8a9791c8bde02a5e9965dc677bdb
ElisaFiorenza <119570190+ElisaFiorenza@users.noreply.github.com>
2024-02-13 20:52:32 +0100

    .

--- anagraficaarray1.js
+++ anagraficaarray1.js
@@ -22,30 +22,54 @@ function dati() {
     if (checkBox.checked == true) {
         aDati[6] = 'Italiano';
     }
+    else
+    {
+        aDati[6] = ' ';
+    }
     var checkBox = document.getElementById("sto");
 
     if (checkBox.checked == true) {
         aDati[7] = 'Storia';
+    }
+    else
+    {
+        aDati[7] = ' ';
     }
     var checkBox = document.getElementById("mat");
 
     if (checkBox.checked == true) {
         aDati[8] = 'Matematica';
     }
+    else
+    {
+        aDati[8] = ' ';
+    }
     var checkBox = document.getElementById("ing");
 
     if (checkBox.checked == true) {
         aDati[9] = 'Inglese';
+    }
+    else
+    {
+        aDati[9] = ' ';
     }
     var checkBox = document.getElementById("inf");
 
     if (checkBox.checked == true) {
         aDati[10] = 'Informatica';
     }
+    else
+    {
+        aDati[10] = ' ';
+    }
     var checkBox = document.getElementById("tps");
 
     if (checkBox.checked == true) {
         aDati[11] = 'TPSIT';
+    }
+    else
+    {
+        aDati[11] = ' ';
     }
     let aDatiJSON = JSON.stringify(aDati);
     localStorage.setItem('dati', aDatiJSON);
